953cccc9b1a1f1a778c1e57c3ebd5f10558f204a
Axel Prevoteaux <axel.prevoteaux@gmail.com>
2019-09-11 17:16:29 +0200

    add association between user and articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
+  has_many :articles
+  before_save { self.email = email.downcase}
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
